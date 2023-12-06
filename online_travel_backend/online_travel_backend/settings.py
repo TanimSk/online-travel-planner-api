3a24c849ce07dd320278e5190345d9e11b03ffc9
tanimsk <sktanim5800@gmail.com>
2023-12-06 09:00:47 +0600

    email

--- online_travel_backend/online_travel_backend/settings.py
+++ online_travel_backend/online_travel_backend/settings.py
@@ -185,6 +185,7 @@ EMAIL_HOST_USER = DEFAULT_FROM_EMAIL = "noreply.service.tanimsk@gmail.com"
 EMAIL_HOST_PASSWORD = "igcosdywfsqodffg"
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+
 # EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 

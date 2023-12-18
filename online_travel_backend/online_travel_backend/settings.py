013d1442a2b8012ddc0be707e63cf04bde99f42b
tanimsk <sktanim5800@gmail.com>
2023-12-18 09:24:02 +0600

    setting

--- online_travel_backend/online_travel_backend/settings.py
+++ online_travel_backend/online_travel_backend/settings.py
@@ -150,7 +150,7 @@ REST_AUTH = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(days=3),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=7),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=30),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": True,

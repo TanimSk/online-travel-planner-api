a88b4749804388c408796dbec2467ad4682a2dbb
Tanim Sk <sktanim5800@gmail.com>
2024-01-07 13:47:36 +0600

    prices

--- online_travel_backend/agent/serializers.py
+++ online_travel_backend/agent/serializers.py
@@ -108,7 +108,7 @@ class RfqSerializer(serializers.ModelSerializer):
             (rfq_service_instance.get("check_in_date", None) is None)
             and (rfq_service_instance.get("check_out_date", None) is None)
         ):
-            date_format = "%Y-%m-%d"
+            date_format = "%Y-%m-%dT%H:%M:%S.%fZ"
             date1 = datetime.strptime(
                 rfq_service_instance.get("check_in_date", None), date_format
             )

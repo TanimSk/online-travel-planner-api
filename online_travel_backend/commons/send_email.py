c4da694ad4db98d7629438d2ac6df7528c49374e
Tanim Sk <sktanim5800@gmail.com>
2024-01-08 20:02:57 +0600

    email

--- online_travel_backend/commons/send_email.py
+++ online_travel_backend/commons/send_email.py
@@ -6,6 +6,7 @@ from administrator.models import Administrator
 from online_travel_backend.settings import DEFAULT_FROM_EMAIL
 
 from agent.models import Agent, Rfq, RfqCategory
+from datetime import datetime
 
 
 class EmailThread(threading.Thread):
@@ -50,12 +51,14 @@ def rfq_created_admin(rfq_instance):
             "agent_num": agent_instance.mobile_no,
             "customer_address": rfq_instance.customer_address,
             "customer_num": rfq_instance.contact_no,
-            "travel_date": rfq_instance.travel_date,
+            "travel_date": datetime.fromisoformat(rfq_instance.travel_date).strftime(
+                "%d/%m/%Y %I:%M %p"
+            ),
             "created_on": rfq_instance.created_on,
             "tracking_id": rfq_instance.tracking_id,
-            "rfq_categories": rfq_categories
+            "rfq_categories": rfq_categories,
         },
     )
-    text_content = strip_tags(html_content)
+    # text_content = strip_tags(html_content)
 
-    send_html_mail("RFQ Created", text_content, emails, DEFAULT_FROM_EMAIL)
+    send_html_mail("RFQ Created", html_content, emails, DEFAULT_FROM_EMAIL)

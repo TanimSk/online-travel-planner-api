594b3eb531e23486c9b8b2c0ef9a7be1b237b419
Tanim Sk <sktanim5800@gmail.com>
2024-01-10 18:39:44 +0600

    customer email

--- online_travel_backend/commons/send_email.py
+++ online_travel_backend/commons/send_email.py
@@ -88,38 +88,36 @@ def rfq_updated_agent(rfq_instance):
         emails = [agent_instance.agent.email]
 
         html_content = render_to_string(
-        "email_notifications/rfq_updated.html",
-        {
-            "customer_name": rfq_instance.customer_name,
-            "customer_address": rfq_instance.customer_address,
-            "customer_num": rfq_instance.contact_no,
-            "travel_date": datetime.fromisoformat(
-                str(rfq_instance.travel_date)
-            ).strftime("%d/%m/%Y %I:%M %p"),
-            "created_on": rfq_instance.created_on,
-            "tracking_id": rfq_instance.tracking_id,
-            "rfq_services": rfq_services,
-        },
-    )
+            "email_notifications/rfq_updated.html",
+            {
+                "customer_name": rfq_instance.customer_name,
+                "customer_address": rfq_instance.customer_address,
+                "customer_num": rfq_instance.contact_no,
+                "travel_date": datetime.fromisoformat(
+                    str(rfq_instance.travel_date)
+                ).strftime("%d/%m/%Y %I:%M %p"),
+                "created_on": rfq_instance.created_on,
+                "tracking_id": rfq_instance.tracking_id,
+                "rfq_services": rfq_services,
+            },
+        )
     else:
         # B2C
         emails = [Customer.objects.get(customer=rfq_instance.customer).customer.email]
         html_content = render_to_string(
-        "email_notifications_customer/rfq_updated.html",
-        {
-            "customer_name": rfq_instance.customer_name,
-            "customer_address": rfq_instance.customer_address,
-            "customer_num": rfq_instance.contact_no,
-            "travel_date": datetime.fromisoformat(
-                str(rfq_instance.travel_date)
-            ).strftime("%d/%m/%Y %I:%M %p"),
-            "created_on": rfq_instance.created_on,
-            "tracking_id": rfq_instance.tracking_id,
-            "rfq_services": rfq_services,
-        },
-    )
-
-    
+            "email_notifications_customer/rfq_updated.html",
+            {
+                "customer_name": rfq_instance.customer_name,
+                "customer_address": rfq_instance.customer_address,
+                "customer_num": rfq_instance.contact_no,
+                "travel_date": datetime.fromisoformat(
+                    str(rfq_instance.travel_date)
+                ).strftime("%d/%m/%Y %I:%M %p"),
+                "created_on": rfq_instance.created_on,
+                "tracking_id": rfq_instance.tracking_id,
+                "rfq_services": rfq_services,
+            },
+        )
 
     send_html_mail("RFQ Updated and Approved", html_content, emails, DEFAULT_FROM_EMAIL)
 
@@ -128,75 +126,125 @@ def rfq_declined_agent(rfq_instance):
     agent_instance = Agent.objects.get(agent=rfq_instance.agent)
     rfq_services = RfqService.objects.filter(rfq_category__rfq=rfq_instance)
 
-    emails = [agent_instance.agent.email]
+    if not agent_instance.pseudo_agent:
+        emails = [agent_instance.agent.email]
+        html_content = render_to_string(
+            "email_notifications/rfq_declined.html",
+            {
+                "customer_name": rfq_instance.customer_name,
+                "customer_address": rfq_instance.customer_address,
+                "customer_num": rfq_instance.contact_no,
+                "travel_date": datetime.fromisoformat(
+                    str(rfq_instance.travel_date)
+                ).strftime("%d/%m/%Y %I:%M %p"),
+                "created_on": rfq_instance.created_on,
+                "tracking_id": rfq_instance.tracking_id,
+                "rfq_services": rfq_services,
+            },
+        )
+    else:
+        emails = [Customer.objects.get(customer=rfq_instance.customer).customer.email]
 
-    html_content = render_to_string(
-        "email_notifications/rfq_declined.html",
-        {
-            "customer_name": rfq_instance.customer_name,
-            "customer_address": rfq_instance.customer_address,
-            "customer_num": rfq_instance.contact_no,
-            "travel_date": datetime.fromisoformat(
-                str(rfq_instance.travel_date)
-            ).strftime("%d/%m/%Y %I:%M %p"),
-            "created_on": rfq_instance.created_on,
-            "tracking_id": rfq_instance.tracking_id,
-            "rfq_services": rfq_services,
-        },
-    )
+        html_content = render_to_string(
+            "email_notifications_customer/rfq_declined.html",
+            {
+                "customer_name": rfq_instance.customer_name,
+                "customer_address": rfq_instance.customer_address,
+                "customer_num": rfq_instance.contact_no,
+                "travel_date": datetime.fromisoformat(
+                    str(rfq_instance.travel_date)
+                ).strftime("%d/%m/%Y %I:%M %p"),
+                "created_on": rfq_instance.created_on,
+                "tracking_id": rfq_instance.tracking_id,
+                "rfq_services": rfq_services,
+            },
+        )
 
     send_html_mail("RFQ Declined", html_content, emails, DEFAULT_FROM_EMAIL)
 
 
-def rfq_confirmed_admin(rfq_instance):
-    agent_instance = Agent.objects.get(agent=rfq_instance.agent)
+def rfq_confirmed_admin(rfq_instance, is_customer=False):
     rfq_services = RfqService.objects.filter(rfq_category__rfq=rfq_instance)
-
     emails = list(
         Administrator.objects.filter(administrator__emailaddress__verified=True)
         .values_list("administrator__email", flat=True)
         .distinct()
     )
 
-    html_content = render_to_string(
-        "email_notifications/rfq_confirmed_admin.html",
-        {
-            "customer_name": rfq_instance.customer_name,
-            "customer_address": rfq_instance.customer_address,
-            "customer_num": rfq_instance.contact_no,
-            "agent_name": agent_instance.agent_name,
-            "agency_name": agent_instance.agency_name,
-            "agent_num": agent_instance.mobile_no,
-            "travel_date": datetime.fromisoformat(
-                str(rfq_instance.travel_date)
-            ).strftime("%d/%m/%Y %I:%M %p"),
-            "created_on": rfq_instance.created_on,
-            "tracking_id": rfq_instance.tracking_id,
-            "rfq_services": rfq_services,
-        },
-    )
+    # send emails to admin
+    if not is_customer:
+        agent_instance = Agent.objects.get(agent=rfq_instance.agent)
+
+        html_content = render_to_string(
+            "email_notifications/rfq_confirmed_admin.html",
+            {
+                "customer_name": rfq_instance.customer_name,
+                "customer_address": rfq_instance.customer_address,
+                "customer_num": rfq_instance.contact_no,
+                "agent_name": agent_instance.agent_name,
+                "agency_name": agent_instance.agency_name,
+                "agent_num": agent_instance.mobile_no,
+                "travel_date": datetime.fromisoformat(
+                    str(rfq_instance.travel_date)
+                ).strftime("%d/%m/%Y %I:%M %p"),
+                "created_on": rfq_instance.created_on,
+                "tracking_id": rfq_instance.tracking_id,
+                "rfq_services": rfq_services,
+            },
+        )
+    else:
+        html_content = render_to_string(
+            "email_notifications_customer/rfq_confirmed_admin.html",
+            {
+                "customer_name": rfq_instance.customer_name,
+                "customer_address": rfq_instance.customer_address,
+                "customer_num": rfq_instance.contact_no,
+                "travel_date": datetime.fromisoformat(
+                    str(rfq_instance.travel_date)
+                ).strftime("%d/%m/%Y %I:%M %p"),
+                "created_on": rfq_instance.created_on,
+                "tracking_id": rfq_instance.tracking_id,
+                "rfq_services": rfq_services,
+            },
+        )
 
     send_html_mail("RFQ Confirmed", html_content, emails, DEFAULT_FROM_EMAIL)
 
     # send email to all vendors
     for service in rfq_services:
         if not service.service.vendor_category.vendor is None:
-            html_content = render_to_string(
-                "email_notifications/rfq_confirmed_vendor.html",
-                {
-                    "agent_name": agent_instance.agent_name,
-                    "agency_name": agent_instance.agency_name,
-                    "agent_num": agent_instance.mobile_no,
-                    "travel_date": datetime.fromisoformat(
-                        str(rfq_instance.travel_date)
-                    ).strftime("%d/%m/%Y %I:%M %p"),
-                    "created_on": rfq_instance.created_on,
-                    "tracking_id": rfq_instance.tracking_id,
-                    "service_category": service.rfq_category.category.category_name,
-                    "service_name": service.service.service_name,
-                    "service_price": service.service_price,
-                },
-            )
+            if not is_customer:
+                html_content = render_to_string(
+                    "email_notifications/rfq_confirmed_vendor.html",
+                    {
+                        "agent_name": agent_instance.agent_name,
+                        "agency_name": agent_instance.agency_name,
+                        "agent_num": agent_instance.mobile_no,
+                        "travel_date": datetime.fromisoformat(
+                            str(rfq_instance.travel_date)
+                        ).strftime("%d/%m/%Y %I:%M %p"),
+                        "created_on": rfq_instance.created_on,
+                        "tracking_id": rfq_instance.tracking_id,
+                        "service_category": service.rfq_category.category.category_name,
+                        "service_name": service.service.service_name,
+                        "service_price": service.service_price,
+                    },
+                )
+            else:
+                html_content = render_to_string(
+                    "email_notifications_customer/rfq_confirmed_vendor.html",
+                    {
+                        "customer_name": agent_instance.agent_name,
+                        "travel_date": datetime.fromisoformat(
+                            str(rfq_instance.travel_date)
+                        ).strftime("%d/%m/%Y %I:%M %p"),
+                        "created_on": rfq_instance.created_on,
+                        "tracking_id": rfq_instance.tracking_id,
+                        "service_category": service.rfq_category.category.category_name,
+                        "service_name": service.service.service_name,
+                        "service_price": service.service_price,
+                    },
+                )
 
             send_html_mail(
                 "RFQ Confirmed",
@@ -206,20 +254,33 @@ def rfq_confirmed_admin(rfq_instance):
             )
 
     # send email to customer
-    html_content = render_to_string(
-        "email_notifications/rfq_confirmed_customer.html",
-        {
-            "agent_name": agent_instance.agent_name,
-            "agency_name": agent_instance.agency_name,
-            "agent_num": agent_instance.mobile_no,
-            "travel_date": datetime.fromisoformat(
-                str(rfq_instance.travel_date)
-            ).strftime("%d/%m/%Y %I:%M %p"),
-            "created_on": rfq_instance.created_on,
-            "tracking_id": rfq_instance.tracking_id,
-            "rfq_services": rfq_services,
-        },
-    )
+    if not is_customer:
+        html_content = render_to_string(
+            "email_notifications/rfq_confirmed_customer.html",
+            {
+                "agent_name": agent_instance.agent_name,
+                "agency_name": agent_instance.agency_name,
+                "agent_num": agent_instance.mobile_no,
+                "travel_date": datetime.fromisoformat(
+                    str(rfq_instance.travel_date)
+                ).strftime("%d/%m/%Y %I:%M %p"),
+                "created_on": rfq_instance.created_on,
+                "tracking_id": rfq_instance.tracking_id,
+                "rfq_services": rfq_services,
+            },
+        )
+    else:
+        html_content = render_to_string(
+            "email_notifications_customer/rfq_confirmed_customer.html",
+            {
+                "travel_date": datetime.fromisoformat(
+                    str(rfq_instance.travel_date)
+                ).strftime("%d/%m/%Y %I:%M %p"),
+                "created_on": rfq_instance.created_on,
+                "tracking_id": rfq_instance.tracking_id,
+                "rfq_services": rfq_services,
+            },
+        )
 
     send_html_mail(
         "RFQ Confirmed", html_content, [rfq_instance.email_address], DEFAULT_FROM_EMAIL

12ab5798c7700e688eeec4257921875a17fc4bdc
Tanim Sk <sktanim5800@gmail.com>
2024-01-11 18:34:05 +0600

    invoice

--- online_travel_backend/agent/templatetags/replace.py
+++ online_travel_backend/agent/templatetags/replace.py
@@ -8,6 +8,12 @@ register = template.Library()
 def replace(value, arg):
     return value.replace(arg, " ")
 
+
 @register.filter
 def mul(value, arg):
     return value * arg
+
+
+@register.filter
+def calculate_total_bill(bill, commission1, commission2):
+    return bill + (bill * commission1 * 0.01) + (bill * commission2 * 0.01)

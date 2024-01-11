8177d0b4ada37ee575c4ea5cae9cf800ea6c76c9
Tanim Sk <sktanim5800@gmail.com>
2024-01-11 18:16:36 +0600

    invoice

--- online_travel_backend/agent/templatetags/replace.py
+++ online_travel_backend/agent/templatetags/replace.py
@@ -7,3 +7,7 @@ register = template.Library()
 @register.filter(name="replace")
 def replace(value, arg):
     return value.replace(arg, " ")
+
+@register.filter
+def mul(value, arg):
+    return value * arg

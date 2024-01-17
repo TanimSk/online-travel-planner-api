e8eaa4fe43474ee592a374f5219a42257e15baa0
Tanim Sk <sktanim5800@gmail.com>
2024-01-17 18:54:25 +0600

    update

--- online_travel_backend/online_travel_backend/settings.py
+++ online_travel_backend/online_travel_backend/settings.py
@@ -185,7 +185,7 @@ REST_USE_JWT = True
 
 ############################ All Auth Config ############################
 
-# EMAIL_USE_TLS = True
+EMAIL_USE_TLS = False
 EMAIL_USE_SSL = True
 # EMAIL_HOST = "smtp.gmail.com"
 EMAIL_HOST = "smtp.zoho.com"
@@ -194,7 +194,8 @@ EMAIL_PORT = 465
 EMAIL_HOST_USER = DEFAULT_FROM_EMAIL = "onghak@miahtrip.com"
 EMAIL_HOST_PASSWORD = "ongshak1@Ota_zoho"
 
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+# EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_BACKEND = "django_smtp_ssl.SSLEmailBackend"
 # EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 

ef7e4f73a2d2b876fda98555cdc9541c002353dc
Tanim Sk <sktanim5800@gmail.com>
2024-01-20 12:00:13 +0600

    main

--- online_travel_backend/vendor/views.py
+++ online_travel_backend/vendor/views.py
@@ -273,7 +273,7 @@ class AdminBillAPI(APIView):
     def get(self, request, format=None, *args, **kwargs):
         if request.GET.get("received") == "true":
             # Getting received payments
-            bills_instance = Bill.objects.filter(status_2="vendor_paid").order_by(
+            bills_instance = Bill.objects.filter(vendor=request.user, status_2="vendor_paid").order_by(
                 "-vendor_paid_on"
             )
             serialized_data = BillServicesSerializer(bills_instance, many=True)

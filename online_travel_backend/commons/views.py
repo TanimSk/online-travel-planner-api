8f7ba21b99523c0eb3239598c55f1ae2b015ed47
Tanim Sk <sktanim5800@gmail.com>
2024-02-04 15:02:07 +0600

    pagination

--- online_travel_backend/commons/views.py
+++ online_travel_backend/commons/views.py
@@ -19,7 +19,7 @@ from rest_framework.pagination import PageNumberPagination
 
 # Pagination Config
 class StandardResultsSetPagination(PageNumberPagination):
-    page_size = 10
+    page_size = 2
     page_size_query_param = "page_size"
     max_page_size = 15
     page_query_param = "p"
